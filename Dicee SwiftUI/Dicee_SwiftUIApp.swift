3cde5d5220dfd72e6d49d114a0f60bc5a11dd40f
Dzmitry Bladyka <dzmitrydistortion@gmail.com>
2023-02-22 01:06:11 +0300

    Initial Commit

---
+++ Dicee SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee SwiftUI
+//
+//  Created by Dzmitry Bladyka on 22.02.23.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
